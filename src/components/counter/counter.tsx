3dbbcde9c8450f463fdb00713433641f48201a39
h-rickyfranco <hrick_gtr@hotmail.com>
2024-03-22 23:31:53 -0300

    counter correção

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import styles from './counter.module.scss'
 
-const Counter = ({title, number}) => {
+interface CounterProps {
+  title: string;
+  number: number;
+}
+
+const Counter: React.FC<CounterProps> = ({title, number}) => {
   return (
     <div className={styles.counter}>
       <p className={styles.counternumber}>{number}</p>
