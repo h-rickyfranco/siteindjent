5973e973a09cfeefb4cffd137e590b6dd2717b4e
h-rickyfranco <hrick_gtr@hotmail.com>
2024-03-22 23:34:37 -0300

    .......

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import styles from './counter.module.scss'
+import React from 'react';
+import styles from './counter.module.scss';
 
 interface CounterProps {
   title: string;
-  number: string;
+  number: string | number; // Alterado para aceitar string ou number
 }
 
-const Counter: React.FC<CounterProps> = ({title, number}) => {
+const Counter: React.FC<CounterProps> = ({ title, number }) => {
   return (
     <div className={styles.counter}>
-      <p className={styles.counternumber}>{number}</p>
+      <p className={styles.counternumber}>{String(number)}</p> {/* Convertendo para string */}
       <h3 className={styles.countertext}>{title}</h3>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
